0654311e6ffdc300ff501e920d7ad1ef0812a53a
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2023-01-05 21:36:37 +0530

    configuring label tag ang giving auto field with initial

--- forms/views.py
+++ forms/views.py
@@ -3,7 +3,8 @@ from .forms import registration
 # Create your views here.
 
 def showFormData(request):
-    fm=registration(auto_id='some_%s')
+    fm=registration(auto_id='some_%s', label_suffix="-",initial={'name':"merenam"})
+    
      #this will set id as some_name ehere name is name of formfield 
      #auto_id can be set as True False also we can add any value like name_field
     return render(request,"forms/registration.html" ,{"form":fm})
