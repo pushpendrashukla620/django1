2c01d23d9a70637a467dc8fe71512801dbbc2e64
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2023-01-05 20:58:59 +0530

    form started and created form

--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .forms import registration
 # Create your views here.
+
+def showFormData(request):
+    fm=registration()
+    return render(request,"forms/registration.html" ,{"form":fm})
