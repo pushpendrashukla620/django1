93dc2e4da644880da90cef71ba3294df1571a125
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2023-01-05 22:15:08 +0530

    loop in form fields

--- forms/forms.py
+++ forms/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class registration(forms.Form):
-    name=forms.CharField(initial="You Name",help_text="please enter your name") #this will set id as some_name ehere name is name of formfield 
-    email=forms.EmailField() #this will set id as email which is name of formfield 
-    text=forms.EmailField(  ) #this will set id as email which is name of formfield 
-    next=forms.EmailField() #this will set id as email which is name of formfield 
+    name=forms.CharField(initial="You Name",help_text="please enter your name") 
+    email=forms.EmailField()   
+    mobile=forms.IntegerField() 
+    key=forms.CharField(widget=forms.HiddenInput)  
