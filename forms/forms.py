c391b7c5cfe4cb4058f058fe6bf11ea2131c2f78
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2023-01-05 21:28:50 +0530

    configuring id field in form

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class registration(forms.Form):
-    name=forms.CharField()
-    email=forms.EmailField()
+    name=forms.CharField() #this will set id as some_name ehere name is name of formfield 
+    email=forms.EmailField() #this will set id as email which is name of formfield 
+    text=forms.EmailField(  ) #this will set id as email which is name of formfield 
+    next=forms.EmailField() #this will set id as email which is name of formfield 
--- forms/views.py
+++ forms/views.py
@@ -3,5 +3,7 @@ from .forms import registration
 # Create your views here.
 
 def showFormData(request):
-    fm=registration()
+    fm=registration(auto_id='some_%s')
+     #this will set id as some_name ehere name is name of formfield 
+     #auto_id can be set as True False also we can add any value like name_field
     return render(request,"forms/registration.html" ,{"form":fm})
