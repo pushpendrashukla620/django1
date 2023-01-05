8ae3aa5626e9ac955f029b7ce24a65d0309735ad
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2023-01-05 21:41:32 +0530

    ordering form fields

--- forms/views.py
+++ forms/views.py
@@ -4,7 +4,7 @@ from .forms import registration
 
 def showFormData(request):
     fm=registration(auto_id='some_%s', label_suffix="-",initial={'name':"merenam"})
-    
+    fm.order_fields(field_order=['name','next','email','text'])
      #this will set id as some_name ehere name is name of formfield 
      #auto_id can be set as True False also we can add any value like name_field
     return render(request,"forms/registration.html" ,{"form":fm})
